ed09a1668745190410119c99644eeef91f0ae40a
Denny Britz <dennybritz@gmail.com>
2016-04-21 13:13:06 -0700

    LSTM code update

---
+++ helpers.py
@@ -0,0 +1,21 @@
+import array
+import numpy as np
+
+
+def load_glove_vectors(filename, vocab=None):
+    """
+    Load glove vectors from a .txt file.
+    Optionally limit the vocabulary to save memory. `vocab` should be a set.
+    """
+    dct = {}
+    vectors = array.array('d')
+    with open(PATH, "r", encoding="utf-8") as f:
+        for idx, line in enumerate(f):
+            tokens = line.split(" ")
+            word = tokens[0]
+            if not vocab or word in vocab:
+                dct[word] = idx
+                vectors.extend(float(x) for x in tokens[1:])
+        word_dim = len(entries)
+        num_vectors = len(dct)
+        return [np.array(vectors).reshape(num_vectors, word_dim), dct]
