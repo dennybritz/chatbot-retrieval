7842a279485699c192cbbf4839bcbac8425c7095
Denny Britz <dennybritz@gmail.com>
2016-07-03 12:28:13 -0700

    Change default optimizer

--- udc_hparams.py
+++ udc_hparams.py
@@ -21,7 +21,7 @@ tf.flags.DEFINE_string("vocab_path", None, "Path to vocabulary.txt file")
 tf.flags.DEFINE_float("learning_rate", 0.001, "Learning rate")
 tf.flags.DEFINE_integer("batch_size", 128, "Batch size during training")
 tf.flags.DEFINE_integer("eval_batch_size", 16, "Batch size during evaluation")
-tf.flags.DEFINE_string("optimizer", "Adagrad", "Optimizer Name (Adam, Adagrad, etc)")
+tf.flags.DEFINE_string("optimizer", "Adam", "Optimizer Name (Adam, Adagrad, etc)")
 
 FLAGS = tf.flags.FLAGS
 
