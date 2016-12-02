0273a84798bfe46b4ea76aa088c16e640b9614e2
Samurais <hailiang.hl.wang@gmail.com>
2016-12-02 10:31:03 +0800

    #29 Fix MetricSpec with prediction_key Issue

--- udc_metrics.py
+++ udc_metrics.py
@@ -2,10 +2,11 @@ import tensorflow as tf
 import functools
 from tensorflow.contrib.learn.python.learn.metric_spec import MetricSpec
 
+
 def create_evaluation_metrics():
     eval_metrics = {}
     for k in [1, 2, 5, 10]:
         eval_metrics["recall_at_%d" % k] = MetricSpec(metric_fn=functools.partial(
             tf.contrib.metrics.streaming_sparse_recall_at_k,
-            k=k), prediction_key="classes")
+            k=k))
     return eval_metrics
